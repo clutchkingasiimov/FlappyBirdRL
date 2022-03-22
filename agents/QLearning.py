c1da23a87c9c2bcc6ecb87314e5bae17f75a689a
clutchkingasiimov <saureyj43@gmail.com>
2022-03-22 13:02:11 +0100

    Changed main and added trainer

--- agents/QLearning.py
+++ agents/QLearning.py
@@ -1,6 +1,16 @@
 import numpy as np 
 
 class QLearning:
+    '''
+    Q-Learning algorithm with epsilon-greedy policy.
+
+
+    Parameters:
+        actions: The action space of the agent 
+        alpha: The step size for the model 
+        gamma: Discount factor 
+        epsilon: Exploration/Exploitation
+    '''
 
     def __init__(self,environment,alpha,gamma,epsilon):
 
@@ -13,6 +23,11 @@ class QLearning:
 
     #Argmax function
     def argmax(self,q_values):
+        '''
+        Finds the argmax of the Q-values with tie-breaking mechanic 
+
+        Note: Do not replace this with np.argmax()
+        '''
         
         top = float('-inf')
         ties = []
@@ -29,6 +44,9 @@ class QLearning:
 
     #Custom Reward function 
     def give_reward(self,done_state):
+        '''
+        Custom reward function with reward of +1 and punishment of -10 
+        '''
         if not done_state:
             return 1
         else:
@@ -36,15 +54,25 @@ class QLearning:
     
     #Helper function for initializing Q-values
     def initialize_q_values(self,state):
+        '''
+        Initiliazes the Q-value using a hash table. 
+        '''
         #Check if the state exists in the table 
         #If not, then load in the initialized value as 0
         self.Q_table[state] = [0,0]
     
-    #Update Q-table with the new values
+    #Update Q-table with the new values of the action-pair for the given state 
+    #Stores the value Q(S,A)
     def update_q_values(self,state,action_pair):
         self.Q_table[state] = action_pair
 
-    def agent_initialize(self,state):
+    def agent_initialize(self,state): 
+        '''
+        Initializes the agent for the training phase 
+
+        Parameters:
+            state: The state variable/vector of the agent on which the action is initialized. 
+        '''
         current_q = self.Q_table[state]
         #Choose a random action using epsilon-greedy strategy 
         random_action_prob = np.random.random()
@@ -58,7 +86,7 @@ class QLearning:
         return action #Return the action that maximizes the Q-value 
     
     def agent_step(self,reward,state):
-            
+
         #Q-update equation
         target_update = reward + self.gamma*np.max(self.Q_table[state])
         Q_prev = self.Q_table[self.prev_state][self.prev_action] #Take the Q-value from the 
---
+++ trainer.py
@@ -0,0 +1,118 @@
+
+from agents.QLearning import QLearning 
+from agents.sarsa import SARSA
+import os, sys
+import time
+
+#SARSA trainer 
+def run_sarsa(env,alpha,gamma,epsilon,episodes):
+
+    sarsa = SARSA(env,alpha,gamma,epsilon)
+    # action = env.observation_space
+
+    #Store the tracking variables in a list
+    score_per_episode = []
+    reward_per_episode = []
+    episode_length = []
+    #Number of episodes to loop 
+    for i in range(episodes):
+    #Initialize the agent 
+        action = env.action_space.sample()
+        obs,_,_,_ = env.step(action) #State, (S)
+        if obs not in sarsa.Q_table:
+            sarsa.initialize_q_values(obs)
+            
+        #Track the reward/episode and the length/episode
+        reward_episode = 0
+        length_ep = 0
+
+        #Initialize the agent and obtain the first action
+        action = sarsa.agent_initialize(obs,eps_decay=0.001) #Initial action, (A)
+
+        # iterate
+        while True:
+            # Select next action
+    #         action = env.action_space.sample()  # for an agent, action = agent.policy(observation)
+            # Appy action and return new observation of the environment
+            obs, _, done, info = env.step(action) 
+            reward = sarsa.give_reward(done) #Obtain reward, (R)
+            if obs not in sarsa.Q_table:
+                sarsa.initialize_q_values(obs)
+                
+            action = sarsa.agent_step(reward,obs,eps_decay=0.001) #Update to next state (S'), Choose next action (A')
+            length_ep += 1 #Episode counter 
+
+            # Render the game
+            os.system("clear")
+            sys.stdout.write(env.render())
+            print(f'Episode {i}')
+            print('SARSA')
+            time.sleep(0.0) # FPS
+
+            # If player is dead break
+            reward_episode += reward
+            if done:
+                sarsa.agent_end(reward)
+                break
+        env.reset()
+        score_per_episode.append(info['score']) #Save the scores 
+        reward_per_episode.append(reward_episode) #Save the rewards 
+        episode_length.append(length_ep) #Save the episodes 
+
+    return episode_length,score_per_episode,reward_per_episode,sarsa.Q_table
+
+#Q-Learning trainer 
+def run_qlearning(env,alpha,gamma,epsilon,episodes):
+
+    ql = QLearning(env,alpha,gamma,epsilon)
+    # action = env.observation_space
+
+    #Store the tracking variables in a list 
+    score_per_episode = []
+    reward_per_episode = []
+    episode_length = []
+    #Number of episodes to loop 
+    for i in range(episodes):
+    #Initialize the agent 
+        action = env.action_space.sample()
+        obs,_,_,_ = env.step(action)
+        if obs not in ql.Q_table:
+            ql.initialize_q_values(obs)
+            
+        #Track the reward/episode and length/episode
+        reward_episode = 0
+        length_ep = 0  
+        # iterate
+        while True:
+            # Select next action
+            action = ql.agent_initialize(obs)
+    #         action = env.action_space.sample()  # for an agent, action = agent.policy(observation)
+            # Appy action and return new observation of the environment
+            obs, _, done, info = env.step(action)
+            reward = ql.give_reward(done)
+            if obs not in ql.Q_table:
+                ql.initialize_q_values(obs)
+                
+            obs = ql.agent_step(reward,obs)
+            length_ep += 1 #Episode counter 
+
+            # Render the game
+            os.system("clear")
+            sys.stdout.write(env.render())
+            print(f'Episode {i}')
+            print('Q-Learing')
+            time.sleep(0.0) # FPS
+
+            # If player is dead break
+            reward_episode += reward
+            if done:
+                ql.agent_end(reward)
+                break
+
+        env.reset()
+        score_per_episode.append(info['score']) #Save the scores 
+        reward_per_episode.append(reward_episode) #Save the rewards 
+        episode_length.append(length_ep) #Save the episodes 
+
+    return episode_length,score_per_episode,reward_per_episode,ql.Q_table
+
